c94c93c0db562c3a8a48a47ffeb7819dd4c1a8a2
Sourcery AI <>
2023-05-12 06:44:29 +0000

    'Refactored by Sourcery'

--- Editor/Linux/Installer/Installer.py
+++ Editor/Linux/Installer/Installer.py
@@ -23,11 +23,11 @@ class WickedDirectories:
 #region Functions
 
 def Install():
-    if WickedDirectories.wickedRootDirectorySelected:    
+    if WickedDirectories.wickedRootDirectorySelected:
         os.system(f'sudo cp {WickedDirectories.wickedRootDirectory}/Editor/Linux/Installer/Distribution/wicked-engine.desktop /usr/share/applications/wicked-engine.desktop')
         os.system(f'sudo cp {WickedDirectories.wickedRootDirectory}/Editor/Linux/Installer/Distribution/wicked-engine.sh /usr/bin/wicked-engine')
-        os.system(f'sudo chmod +x {WickedDirectories.wickedRootDirectory}/Editor/Linux/Installer/Distribution/wicked-engine.sh')       
-        os.system(f'sudo chmod +x /usr/bin/wicked-engine')
+        os.system(f'sudo chmod +x {WickedDirectories.wickedRootDirectory}/Editor/Linux/Installer/Distribution/wicked-engine.sh')
+        os.system('sudo chmod +x /usr/bin/wicked-engine')
 
         print("Finished installing!")
     else:
@@ -41,7 +41,7 @@ def SelectDirectory():
 
 if __name__ == "__main__":
     #region Tk Interface variables
-    
+
     window = TkBootstrap.Window()
     window.title("Wicked Engine Installer")
     window.resizable(False, False)
@@ -53,7 +53,7 @@ if __name__ == "__main__":
     #endregion
 
     #region Tk Interface widgets
-    
+
     button_install = TkBootstrap.Button(text = "start install", bootstyle = 'success', command = Install)
     button_path = TkBootstrap.Button(text = "select source folder", bootstyle = 'default', command = SelectDirectory)
     checkButton_denoiser = TkBootstrap.Checkbutton(text = "add denoising", bootstyle = 'outline-toolbutton')
@@ -65,7 +65,7 @@ if __name__ == "__main__":
 
     #region Window changes, widget packing/placing, etc.
 
-    window.geometry(str(screenWidth) + 'x' + str(screenHeight)) # Needs to be strings for some reason?
+    window.geometry(f'{screenWidth}x{screenHeight}')
 
     button_install.place(relx = 0.5, rely = 0.75, anchor = CENTER)
     button_path.place(relx = 0.5, rely = 0.5, anchor = CENTER)
